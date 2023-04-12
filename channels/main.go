e43d93a5c856029f74b01cb7b9472018ea60e08e
Maciej Kaliś <mackal1993@gmail.com>
2023-04-12 19:29:52 +0200

    Adding website status checker program in channels/

---
+++ channels/main.go
@@ -0,0 +1,40 @@
+// This package is a website status checker
+// crated to practice Go Routines and Channels usage
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"https://go.dev",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- "Yep its up"
+}
